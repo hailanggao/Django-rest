d0376770319c969ef03fd63a2bc1a075a00f23c3
Gordon Gao <gordon.gao@watco.com>
2024-03-06 16:02:52 +0800

    update startup, change env to pipenv

--- test-app/server/api/serializers/product_serializer.py
+++ test-app/server/api/serializers/product_serializer.py
@@ -4,12 +4,12 @@ from ..models.products import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # this will custimize the dicount fieldname and retrieve this field method with get prefix: et_discount
+    # this will custimize the dicount fieldname and retrieve this field method with get prefix: get_discount
     discount = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Product
-        fields = ["title", "content", "price", "sale_price", "discount"]
+        fields = ["id", "title", "content", "price", "sale_price", "discount"]
 
     # This will call the Product.get_discount method and return it to custimized field discount above
     def get_discount(self, obj):
