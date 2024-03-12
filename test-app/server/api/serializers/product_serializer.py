2e13028fd96381885ba8d6b1c1f1220a36288f08
Gordon Gao <gordon.gao@watco.com>
2024-03-12 11:00:58 +0800

    Add url on list, and name viewname

--- test-app/server/api/serializers/product_serializer.py
+++ test-app/server/api/serializers/product_serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from ..models.products import Product
 
@@ -6,10 +7,31 @@ from ..models.products import Product
 class ProductSerializer(serializers.ModelSerializer):
     # this will custimize the dicount fieldname and retrieve this field method with get prefix: get_discount
     discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)  # one way
+    url = serializers.HyperlinkedIdentityField(
+        view_name="product-detail", lookup_field="pk"
+    )  # preferred way
 
     class Meta:
         model = Product
-        fields = ["id", "title", "content", "price", "sale_price", "discount"]
+        fields = [
+            "url",
+            "edit_url",
+            "id",
+            "title",
+            "content",
+            "price",
+            "sale_price",
+            "discount",
+        ]
+
+    def get_edit_url(self, obj):
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse(
+            "product-edit", kwargs={"pk": obj.pk}, request=request
+        )  # product-edit is the url view name in the url patterns
 
     # This will call the Product.get_discount method and return it to custimized field discount above
     def get_discount(self, obj):
--- test-app/server/api/urls.py
+++ test-app/server/api/urls.py
@@ -7,8 +7,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("", views.api_home),
     path("auth/", obtain_auth_token),
-    path("products/", product_views.product_list_create_view),
-    path("products/<int:pk>/", product_views.product_detail_view),
-    path("products/<int:pk>/update/", product_views.product_update_view),
+    path("products/", product_views.product_list_create_view, name="product-list"),
+    path("product/<int:pk>/", product_views.product_detail_view, name="product-detail"),
+    path(
+        "product/<int:pk>/update/",
+        product_views.product_update_view,
+        name="product-edit",
+    ),
     path("products/<int:pk>/delete/", product_views.product_delete_view),
 ]
