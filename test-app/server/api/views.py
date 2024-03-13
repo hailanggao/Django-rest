baf94cb7b0e35e623255f0ac0d4b0449ad973960
Gordon Gao <gordon.gao@watco.com>
2024-03-13 14:48:21 +0800

    api_home get request

--- test-app/server/api/views.py
+++ test-app/server/api/views.py
@@ -5,36 +5,36 @@ from .models.products import Product
 from .serializers.product_serializer import ProductSerializer
 
 
-@api_view(["POST"])
-def api_home(request, *args, **kwargs):
-    """
-    DRF API View
-    """
-    # request.data => Serializer verify if it meets to serializer requirements => verify the requirements of the model
-    serializer = ProductSerializer(data=request.data)
-
-    if serializer.is_valid(raise_exception=True):
-        # instance = (
-        #     serializer.save()
-        # )  # this step will save the data and return the instance itself
-        return Response(serializer.data)
-    return Response({"invalid": "not good data"}, status=400)
-
-
-# @api_view(["GET"])
+# @api_view(["POST"])
 # def api_home(request, *args, **kwargs):
 #     """
 #     DRF API View
 #     """
-#     instance = Product.objects.all().order_by("?").first()
-#     data = {}
-#     if instance:
-#         data = ProductSerializer(instance).data
-#         # data = model_to_dict(instance, fields=["id", "title", "price", "sale_price"])
-#         # data["ttile"] = model_data.title
-#         # data["content"] = model_data.content
-#         # data["price"] = model_data.price
-#     return Response(data)
+#     # request.data => Serializer verify if it meets to serializer requirements => verify the requirements of the model
+#     serializer = ProductSerializer(data=request.data)
+
+#     if serializer.is_valid(raise_exception=True):
+#         # instance = (
+#         #     serializer.save()
+#         # )  # this step will save the data and return the instance itself
+#         return Response(serializer.data)
+#     return Response({"invalid": "not good data"}, status=400)
+
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?")
+    data = {}
+    if instance:
+        data = ProductSerializer(instance, context={"request": request}, many=True).data
+        # data = model_to_dict(instance, fields=["id", "title", "price", "sale_price"])
+        # data["ttile"] = model_data.title
+        # data["content"] = model_data.content
+        # data["price"] = model_data.price
+    return Response(data)
 
 
 # def api_home(request, *args, **kwargs):
